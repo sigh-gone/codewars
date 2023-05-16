dd4c933f1ef1642a1aacdd2c80e5b1e8b04a41a8
Al <112661044+sigh-gone@users.noreply.github.com>
2023-05-16 11:42:39 -0500

    Delete codewars/target directory

--- codewars/target/debug/deps/codewars-913c97e111a51ffb.d
+++
@@ -1,5 +0,0 @@
-/Users/a1/Projects/codewars/codewars/target/debug/deps/codewars-913c97e111a51ffb.rmeta: src/main.rs
-
-/Users/a1/Projects/codewars/codewars/target/debug/deps/codewars-913c97e111a51ffb.d: src/main.rs
-
-src/main.rs:
